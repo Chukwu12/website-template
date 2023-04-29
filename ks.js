8226fb490d10156f358e1dd3925ae89f204feec1
okey <39450490+Chukwu12@users.noreply.github.com>
2023-04-29 10:41:30 -0400

    Update ks.js

--- ks.js
+++ ks.js
@@ -4,3 +4,17 @@
         prevEl: ".swiper-button-prev",
       },
     });
+
+
+    let swiperProducts = new Swiper(".products-container", {
+      spaceBetween: 32,
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      loop: true;
+      
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
