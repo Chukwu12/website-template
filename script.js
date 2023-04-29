8eae49950efd1f716181c828aa15d04d08056f6a
okey <39450490+Chukwu12@users.noreply.github.com>
2023-04-29 12:50:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,18 @@
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
       },
+        breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 50,
+        },
+      },
     });
