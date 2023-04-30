3a3547447b19ecc2a3718330fbd5535cf6abd265
okey <39450490+Chukwu12@users.noreply.github.com>
2023-04-30 11:41:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
 
-    let swiperProducts = new Swiper(".products-container", {
-      spaceBetween: 32,
-      grabCursor: true,
-      centeredSlides: true,
-      slidesPerView: 'auto',
-      loop: true,
-      
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-        breakpoints: {
-        1024: {
-     
-          spaceBetween: 72,
-        },
-      },
-    });
-
 
 
                
