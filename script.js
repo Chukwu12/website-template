8569e2c4d9d2f6e415c9487d94a5a96f2fa5cf11
okey <39450490+Chukwu12@users.noreply.github.com>
2023-04-30 11:36:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,25 +19,7 @@
     });
 
 
-//*=============== Initialize Swiper =======================*//
-<script>
-  var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 0,
-      stretch: 0,
-      depth: 300,
-      modifier: 1,
-      slideShadows: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
-</script>
+
                
 
 
