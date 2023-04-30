dc41757873b91f6e6d4ee756ad9d6c5731f82c05
okey <39450490+Chukwu12@users.noreply.github.com>
2023-04-30 11:06:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,7 @@
     });
 
 
-// Change background color for header //
+/*=============== CHANGE BACKGROUND HEADER ===============*/
 const scrollHeader = () =>{
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
