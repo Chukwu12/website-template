669783b6d752738ee1ebbef9e91ad8001cf5060f
Okechukwu Egeruoh <oegeruoh@gmail.com>
2023-05-07 00:45:40 -0400

    updated

--- js/script.js
+++ js/script.js
@@ -69,15 +69,17 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
 
-
-      
-
-    
-          
-		
-		
-		
-		
-		
-
-   
+// About Card //
+var swiper = new Swiper('.blog-slider', {
+	spaceBetween: 30,
+	effect: 'fade',
+	loop: true,
+	mousewheel: {
+	  invert: false,
+	},
+	// autoHeight: true,
+	pagination: {
+	  el: '.blog-slider__pagination',
+	  clickable: true,
+	}
+  });
