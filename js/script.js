6a02fcffcb6bd9bc4d515390fdf9a31bd9b34c16
okey <39450490+Chukwu12@users.noreply.github.com>
2023-05-13 17:24:21 +0000

    finished tabs

--- js/script.js
+++ js/script.js
@@ -83,3 +83,24 @@ var swiper = new Swiper('.blog-slider', {
 	  clickable: true,
 	}
   });
+
+
+//   Tabs //
+let tabHeader = document.getElementsByClassName("tab-header")[0];
+let tabBody = document.getElementsByClassName("tab-body")[0];
+
+let tabsPane = tabHeader.getElementsByTagName("div");
+
+for (let i=0; i < tabsPane.length; i++) {
+	tabsPane[i].addEventListener("click",function(){
+		tabHeader.getElementsByClassName("active")
+		[0].classList.remove("active");
+		tabsPane[i].classList.add("active");
+		tabBody.getElementsByClassName("active")
+		[0].classList.remove("active");
+		tabBody.getElementsByTagName("div")
+		[i].classList.add("active");
+
+		tabIndicator.style.left = `calc(calc(100% / 4) * ${i})`;
+	});
+}
