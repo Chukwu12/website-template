c2c2558301623120f67ba4ce6613cbed1c0fed6b
okey <39450490+Chukwu12@users.noreply.github.com>
2023-05-18 11:30:30 +0000

    update

--- js/script.js
+++ js/script.js
@@ -115,3 +115,14 @@ for (let th = 0; th < tab_items.length; th++) {
   document.querySelector('.tab-filter-item-container').style.height =
     tab_items[th].scrollHeight + 'px';
 }
+
+
+
+
+function readMore() {
+  var expandInfo = document.getElementById("more-info-js");
+  var mainHeadings = document.getElementById("main-headings-js");
+  
+  mainHeadings.style.transform = "scale(0.7)";
+  expandInfo.style.height = "350px";
+} 
