86e68ce8eaf3b8e962e57f3d61abd2cd1f673f05
okey <39450490+Chukwu12@users.noreply.github.com>
2023-05-18 02:46:21 +0000

    update

--- js/script.js
+++ js/script.js
@@ -86,25 +86,7 @@ var swiper = new Swiper('.blog-slider', {
 
 
 //   Tabs //
-let tabHeader = document.getElementsByClassName("tabs-header")[0];
-let tabIndicator = document.getElementsByClassName("tab-indicator")[0];
-let tabBody = document.getElementsByClassName("tab-body")[0];
 
-let tabsPane = tabHeader.getElementsByTagName("div");
-
-for (let i = 0; i < tabsPane.length; i++) {
-	tabsPane[i].addEventListener("click",function(){
-		tabHeader.getElementsByClassName("active")
-		[0].classList.remove("active");
-		tabsPane[i].classList.add("active");
-		tabBody.getElementsByClassName("active")
-		[0].classList.remove("active");
-		tabBody.getElementsByTagName("div")
-		[i].classList.add("active");
-
-		tabIndicator.style.left = `calc(calc(100% / 3) * ${i})`;
-	});
-}
 
 
 let filter_btn = document.querySelectorAll('.filter-btn');
