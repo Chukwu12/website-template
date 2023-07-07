d0851e146fc64867888cc4ae8a89b1ae1ae2aeea
Okechukwu Egeruoh <oegeruoh@gmail.com>
2023-07-07 09:54:55 -0400

    update

--- js/script.js
+++ js/script.js
@@ -117,14 +117,7 @@ function readMore() {
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const navLinkEls = document.querySelectorAll('.nav__link');
-const windowPathname = window.location.pathname;
-    
-navLinkEls.forEach(navLinkEl => {
-if (navLinkEl.href.inculdes(windowPathname)) {
-  navLinkEl.classList.add('active');
-}
-});
+
 
 
 
